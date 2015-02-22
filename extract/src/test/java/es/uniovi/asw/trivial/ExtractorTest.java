ba8aed8f55685f3e02dd5ebbb44fcfc6461dc4b0
Jairo López Soto <jairo21190@hotmail.com>
2015-02-22 23:10:13 +0100

    Pruebas para el parser del gift

--- extract/src/test/java/es/uniovi/asw/trivial/ExtractorTest.java
+++ extract/src/test/java/es/uniovi/asw/trivial/ExtractorTest.java
@@ -10,14 +10,40 @@ import es.uniovi.asw.trivial.preguntas.Pregunta;
 import es.uniovi.asw.trivial.saver.MongoSaver;
 import es.uniovi.asw.trivial.serializers.JSONSerializer;
 
-public class ExtractorTest {
+public class ExtractorTest {	
 
+	@Test
+	public void emptyList(){ // Comprueba que la lista no está vacía
+		List<Pregunta> preguntasGIFT = new GIFTParser().parse("src/main/resources/preguntas.gift");
+		assertEquals(0, preguntasGIFT.size());
+	}
+	
+	@Test
+	public void preguntasNull(){ //Comprueba que no hay nulos en la lista
+		List<Pregunta> preguntasGIFT = new GIFTParser().parse("src/main/resources/preguntas.gift");
+		
+		for(int i = 0; i < preguntasGIFT.size(); i++){
+			Pregunta pregunta = preguntas.get(i);
+			assertNull("Mensaje OPCIONAL por si es null", pregunta);
+		}
+	}
+	
+	@Test
+	public void emptyQuest(){ //Comprueba que no hay preguntas vacias
+		List<Pregunta> preguntasGIFT = new GIFTParser().parse("src/main/resources/preguntas.gift");
+		
+		for(int i = 0; i < preguntasGIFT.size(); i++){
+			Pregunta pregunta = preguntas.get(i);
+			assertTrue(pregunta.getQueryText()!="");
+		}
+	}	
+	
 	@Test
 	public void emptyExtractor() {
 		// GIFT
 		List<Pregunta> preguntasGIFT = new GIFTParser().parse("src/main/resources/preguntas.gift");
 		String preguntasGIFTenJSON = new JSONSerializer().serialize(preguntasGIFT);
-
+		
 		System.out.println(preguntasGIFTenJSON);
 
 		// XML
