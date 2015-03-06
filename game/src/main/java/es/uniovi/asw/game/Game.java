10c90f8effbd0dc410e767fed1077cce3ae5c06b
Daniel García <dani-garcia@users.noreply.github.com>
2015-03-06 18:16:42 +0100

    Añadido el proyecto para la segunda entrega

---
+++ game/src/main/java/es/uniovi/asw/game/Game.java
@@ -0,0 +1,7 @@
+package es.uniovi.asw.game;
+
+public class Game {
+	public static void main(String[] args) {
+		System.out.println("Juego");
+	}
+}
---
+++ game/src/test/java/es/uniovi/asw/game/GameTest.java
@@ -0,0 +1,13 @@
+package es.uniovi.asw.game;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GameTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
