2eb28e2ffa6d422d11367437e781e1282503dfa6
Daniel García <dani-garcia@users.noreply.github.com>
2015-03-09 17:01:13 +0100

    Añadida clase encargada de obtener preguntas de la base de datos

---
+++ game/src/main/java/es/uniovi/asw/questions/MongoQuestions.java
@@ -0,0 +1,58 @@
+package es.uniovi.asw.questions;
+
+import java.net.UnknownHostException;
+import java.util.Random;
+
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+
+import es.uniovi.asw.model.Category;
+import es.uniovi.asw.model.Pregunta;
+
+public class MongoQuestions {
+	private static final String DEFAULT_MONGO = "localhost:27017";
+	private String server;
+
+	public MongoQuestions() {
+		this(DEFAULT_MONGO);
+	}
+
+	public MongoQuestions(String server) {
+		this.server = server;
+	}
+
+	public Pregunta getQuestion(Category categ) {
+		try {
+			// Creamos la conexion
+			MongoClient mongo = new MongoClient(server);
+
+			// Obtenemos la base de datos y la coleccion
+			DB db = mongo.getDB("trivial");
+			DBCollection collection = db.getCollection("questions");
+
+			// Creamos un objeto para buscar por categoría
+			BasicDBObject query = new BasicDBObject("category", categ.name());
+
+			// Obtenemos un numero al azar entre 0 y el número de preguntas
+			int count = (int) collection.count(query);
+			int toSkip = new Random().nextInt(count);
+
+			// Obtenemos la pregunta en la posicion dada
+			DBObject pregunta = collection.find(query).skip(toSkip).next();
+
+			// Y la parseamos desde JSON
+			return new Gson().fromJson(pregunta.toString(), Pregunta.class);
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
