fc91da59c47dc5da8ad66ba1b88c85d1c54be7ab
Daniel García <dani-garcia@users.noreply.github.com>
2015-03-10 20:07:28 +0100

    Arreglados problemas de cast

--- game/src/main/java/es/uniovi/asw/game/Game.java
+++ game/src/main/java/es/uniovi/asw/game/Game.java
@@ -12,22 +12,19 @@ import es.uniovi.asw.model.Tablero;
 import es.uniovi.asw.questions.MongoQuestions;
 
 public class Game {
-	
+
 	private MongoQuestions mongoQuestions = new MongoQuestions();
-	
-	private List<Player> players = new ArrayList<Player>();;
+
+	private List<Player> players = new ArrayList<Player>();
 	private Graph<Casilla> tablero;
-	
+
 	public static void main(String[] args) {
 		System.out.println("Juego");
 	}
-	
-	public Game(int var){
-		tablero =Tablero.getTablero(var);
-	}
-	
-	
-	
+
+	public Game(int var) {
+		tablero = Tablero.getTablero(var);
+	}	
 	
 	public Graph<Casilla> getTablero() {
 		return tablero;
@@ -35,39 +32,36 @@ public class Game {
 
 	/**
 	 * Metodo que añade un jugador a l apartida y lo coloca en la casilla de salida
-	 * @param p jugador nuevo. 
+	 * @param p jugador nuevo.
 	 */
-	public void addPlayer(Player p){
+	public void addPlayer(Player p) {
 		players.add(p);
-		p.setPosition(tablero.getNodes()[0]);
+		p.setPosition(tablero.getNode(0));
 	}
-	
+
 	/**
 	 * Cambia la posicion de un jugador al final del turno
-	 * @param player, jugador que va  a cambiar de posicion
-	 * @param c, nueva casilla en la que se va a situar el jugador. 
+	 * @param player, jugador que va a cambiar de posicion
+	 * @param c, nueva casilla en la que se va a situar el jugador.
 	 */
-	public void movePlayer(Player player, Casilla c){
+	public void movePlayer(Player player, Casilla c) {
 		player.setPosition(c);
-		
 	}
-	
+
 	/**
 	 * Metodo que obtiene una pregunta a partir de una casilla
 	 * @param c, casilla en la que se encuentra el jugador que necesita la pregunta
 	 * @return pregunta acorde a la categoria de la casilla
 	 */
-	public Pregunta getPregunta(Casilla c){
+	public Pregunta getPregunta(Casilla c) {
 		return mongoQuestions.getQuestion(c.getCategoria());
 	}
-	
+
 	/**
 	 * Devuelve la tirada del dado aleatoriamente
 	 * @return numero del dado entre 1 y 6
 	 */
-	public int getTirada(){
-		return new Random().nextInt(6) +1;
+	public int getTirada() {
+		return new Random().nextInt(6) + 1;
 	}
-	
-
 }
--- game/src/test/java/es/uniovi/asw/game/GameTest.java
+++ game/src/test/java/es/uniovi/asw/game/GameTest.java
@@ -1,6 +1,6 @@
 package es.uniovi.asw.game;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
@@ -11,18 +11,31 @@ import es.uniovi.asw.model.Graph;
 
 public class GameTest {
 
-    @Test
-    public void test() {
-        assertEquals(1, 1);
-        
-        Game game = new Game(1);
-        
-        Graph<Casilla> tablero= game.getTablero();
-        
-       // Casilla c = tablero.getNodes()[0];//-->error de cast
-       
-       // List<Casilla> lista = tablero.getNodesDestino(c, 2.0);
-        
-        //assertEquals(true, lista.contains(tablero.getNodes()[2]));
-    }
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+
+		Game game = new Game(1);
+
+		Graph<Casilla> tablero = game.getTablero();
+
+		Casilla c = ((Casilla) tablero.getNode(0));
+
+		List<Casilla> lista = tablero.getNodesDestino(c, 2.0);
+
+		assertTrue(lista.contains(tablero.getNode(2)));
+		assertTrue(lista.contains(tablero.getNode(40)));
+
+		// Todavia no están los radios centrales
+		// assertTrue(lista.contains(tablero.getNode(43)));
+
+		List<Casilla> lista2 = tablero.getNodesDestino(c, 6.0);
+
+		assertTrue(lista2.contains(tablero.getNode(6)));
+		assertTrue(lista2.contains(tablero.getNode(36)));
+
+		// Todavia no están los radios centrales
+		// assertTrue(lista2.contains(tablero.getNode(72)));
+
+	}
 }
