27c31a9429f9b9f5c0bbac45d8b32361631b72d4
UO180365 <raul_fm_@hotmail.com>
2015-03-15 17:01:02 +0100

    EstadisticasJugadorJdbcDao

--- game/src/main/java/es/uniovi/asw/persistence/EstadisticasJugadorDao.java
+++ game/src/main/java/es/uniovi/asw/persistence/EstadisticasJugadorDao.java
@@ -2,7 +2,8 @@ package es.uniovi.asw.persistence;
 
 public interface EstadisticasJugadorDao {
 
-	
+	void setConexion(Connection conexion);
+	List<Map<String, Object>> findAll();
 	
 	
 }
--- game/src/main/java/es/uniovi/asw/persistence/impl/EstadisticasJugadorJdbcDao.java
+++ game/src/main/java/es/uniovi/asw/persistence/impl/EstadisticasJugadorJdbcDao.java
@@ -3,5 +3,57 @@ package es.uniovi.asw.persistence.impl;
 import es.uniovi.asw.persistence.EstadisticasJugadorDao;
 
 public class EstadisticasJugadorJdbcDao implements EstadisticasJugadorDao{
+	
+	private Connection conexion;
+	
+	@Override
+	public void setConexion(Connection conexion){
+		this.conexion = conexion;
+	}
+	
+	/**
+	 * Carga el resultado de un ResultSet
+	 */
+	private Map<String, Object> load(ResultSet rs) throws SQLException {
+
+		Map<String, Object> estadisticaJugador = new HashMap<>();
+
+		categoria.put("IDJUGADOR", rs.getLong("IDJUGADOR"));
+		categoria.put("IDPREGUNTA", rs.getString("IDPREGUNTA"));
+		categoria.put("ACIERTOS", rs.getDouble("ACIERTOS"));
+		categoria.put("FALLOS", rs.getDouble("FALLOS"));
+
+		return estadisticaJugador;
+	}
+	
+	/**
+	 * Muestra todas las estadisticasJugador
+	 */
+	@Override
+	public List<Map<String, Object>> findAll() {
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+
+			ps = conexion.prepareStatement(Conf.get("SQL_ESTADISTICASJUGADOR_FINDALL"));
+
+			rs = ps.executeQuery();
+			List<Map<String, Object>> estadisticasJugador = new ArrayList<>();
+			while (rs.next()) {
+
+				Map<String, Object> estadisticaJugador = load(rs);
+
+				estadisticasJugador.add(estadisticaJugador);
+			}
+
+			return estadisticasJugador;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			Jdbc.close(rs, ps);
+		}
+	}
 
 }
