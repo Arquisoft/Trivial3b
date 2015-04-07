7f13a80a71992b1ce7c88dd001e8ca51072640f6
alcalaperez <uo226321@uniovi.es>
2015-04-07 21:22:46 +0200

    Ampliacion de estadisticas

--- game/src/main/java/es/uniovi/asw/view/gui/TablaEstadistica.java
+++ game/src/main/java/es/uniovi/asw/view/gui/TablaEstadistica.java
@@ -15,17 +15,26 @@ import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 
 import es.uniovi.asw.modelo.persistence.impl.EstadisticasJugadorJdbcDao;
+import es.uniovi.asw.modelo.persistence.impl.PreguntaJdbcDao;
 import es.uniovi.asw.util.FileUtil;
 import es.uniovi.asw.util.Jdbc;
 import es.uniovi.asw.view.components.ModeloNoEditable;
+
+import javax.swing.JTabbedPane;
 
 public class TablaEstadistica extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
-	private ModeloNoEditable modeloTabla;
+	private ModeloNoEditable tablaTodo;
+	private ModeloNoEditable tablaMasAcertadas;
+	private ModeloNoEditable tablaMasFalladas;
 	private JButton btnNewButton;
+	private JScrollPane scrollPane_1;
+	private JScrollPane scrollPane_2;
+	private JTable table_1;
+	private JTable table_2;
 
 	/**
 	 * Create the frame.
@@ -40,22 +49,48 @@ public class TablaEstadistica extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		tablaTodo = new ModeloNoEditable(new String[] { "Id jugador",
+				"Id pregunta", "Aciertos", "Fallos" }, 0);
+		tablaMasAcertadas = new ModeloNoEditable(new String[] { "Id pregunta",
+				"Aciertos", "Fallos" }, 0);
+		tablaMasFalladas = new ModeloNoEditable(new String[] { "Id pregunta",
+				"Aciertos", "Fallos" }, 0);
+		contentPane.add(getBtnNewButton());
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 434, 278);
+		contentPane.add(tabbedPane);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(5, 5, 424, 267);
-		contentPane.add(scrollPane);
-		modeloTabla = new ModeloNoEditable(new String[] { "Id jugador",
-				"Id pregunta", "Aciertos", "Fallos" }, 0);
-		meterTablas();
+		tabbedPane.addTab("Estadisticas totales", null, scrollPane, null);
 
 		table = new JTable();
-		table.setModel(modeloTabla);
+		table.setModel(tablaTodo);
 		table.setBackground(Color.WHITE);
 		scrollPane.setViewportView(table);
-		contentPane.add(getBtnNewButton());
+
+		scrollPane_1 = new JScrollPane();
+		tabbedPane.addTab("Preguntas mas acertadas", null, scrollPane_1, null);
+		
+		table_1 = new JTable();
+		table_1.setModel(tablaMasAcertadas);
+		table_1.setBackground(Color.WHITE);
+		scrollPane_1.setViewportView(table_1);
+
+		scrollPane_2 = new JScrollPane();
+		tabbedPane.addTab("Preguntas mas falladas", null, scrollPane_2, null);
+		
+		table_2 = new JTable();
+		table_2.setModel(tablaMasAcertadas);
+		table_2.setBackground(Color.WHITE);
+		scrollPane_2.setViewportView(table_2);
+
+		meterTablasTodo();
+		meterTablasAcertadas();
+		meterTablasFalladas();
 	}
 
-	public void meterTablas() {
+	public void meterTablasTodo() {
 		EstadisticasJugadorJdbcDao dao = new EstadisticasJugadorJdbcDao();
 		try {
 			dao.setConexion(Jdbc.getConnection());
@@ -72,9 +107,47 @@ public class TablaEstadistica extends JFrame {
 			line[2] = estadisticas.get(i).get("ACIERTOS");
 			line[3] = estadisticas.get(i).get("FALLOS");
 
-			modeloTabla.addRow(line);
+			tablaTodo.addRow(line);
 
 		}
+	}
+	
+	public void meterTablasAcertadas() {
+		PreguntaJdbcDao dao = new PreguntaJdbcDao();
+		try {
+			dao.setConnection(Jdbc.getConnection());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Map<String, Object> estadisticas = dao.getMasAcertada();
+		Object[] line = new Object[3];
+
+			line[0] = estadisticas.get("IDPREGUNTA");
+			line[1] = estadisticas.get("ACIERTOS");
+			line[2] = estadisticas.get("FALLOS");
+
+			tablaMasAcertadas.addRow(line);
+
+		
+	}
+	
+	public void meterTablasFalladas() {
+		PreguntaJdbcDao dao = new PreguntaJdbcDao();
+		try {
+			dao.setConnection(Jdbc.getConnection());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Map<String, Object> estadisticas = dao.getMasFallada();
+		Object[] line = new Object[3];
+
+			line[0] = estadisticas.get("IDPREGUNTA");
+			line[1] = estadisticas.get("ACIERTOS");
+			line[2] = estadisticas.get("FALLOS");
+
+			tablaMasFalladas.addRow(line);
 	}
 
 	private JButton getBtnNewButton() {
