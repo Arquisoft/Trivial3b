3fcc8e162a114c4e157045a0d6a63609f4d1b333
josemanuelgarcia <josececos@gmail.com>
2015-04-12 16:36:10 +0200

    Ejemplo de routes/Aplication para el registro de usuarios

--- play-core/app/controllers/Application.java
+++ play-core/app/controllers/Application.java
@@ -10,5 +10,21 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    
+    
+  /** 
+   * Ejemplo de los metodos referenciados en routes
+   *  public static void createUser(User newUser) {
+        newUser.save();
+        user(newUser.id);
+    }
+
+    public static void updateUser(Long id, User user) {
+        User dbUser = User.findById(id);
+        dbUser.updateDetails(user); // some model logic you would write to do a safe merge
+        dbUser.save();
+        user(id);
+    }
+    */
 
 }
