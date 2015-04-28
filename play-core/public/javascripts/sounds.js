46ec69ae7b99a327952f047356279433b621d317
alcalaperez <uo226321@uniovi.es>
2015-04-28 19:39:52 +0200

    Estilo para el tablero de juego
    Estilo principal y prototipo de sonidos y mensajes cuando se acierta o
    se falla.

---
+++ play-core/public/javascripts/sounds.js
@@ -0,0 +1,23 @@
+/*!
+ * Bootstrap v3.3.2 (http://getbootstrap.com)
+ * Copyright 2011-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+	var acierto = new Audio();
+	var fallo = new Audio();
+
+	acierto.src = "/sounds/Acierto.mp3";
+	fallo.src = "/sounds/Fallo.mp3";
+
+	function playAcierto(){
+		acierto.play(); // Play button sound now
+	    alert("Has acertado :D");
+	  
+	}
+	
+	function playFallo(){
+		fallo.play(); // Play button sound now
+	    alert("Ohhhhhh! Has Fallado :(");
+	  
+	}
+}
