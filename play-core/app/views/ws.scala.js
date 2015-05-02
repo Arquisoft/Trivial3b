3266c6082b1ce886a3b029a277f2d269ca0d0974
Cristian Navia <cristiannavia3@hotmail.com>
2015-05-02 16:41:03 +0200

    Ya no se pierde el chat al hacer refresh

--- play-core/app/views/ws.scala.js
+++ play-core/app/views/ws.scala.js
@@ -1,7 +1,12 @@
 var app = angular.module('index', [])
 
 			app.run(function($rootScope, $interval) {
-				var datosGlobal="";
+			if(sessionStorage.getItem("datosGlobales")!=null){
+		var datosGlobales=sessionStorage.getItem("datosGlobales");
+				}
+			else{
+				var datosGlobales="";
+			}
 $(function(){
     // get websocket class, firefox has a different way to get it
     var WS = window['MozWebSocket'] ? window['MozWebSocket'] : WebSocket;
@@ -11,8 +16,7 @@ $(function(){
     
     var writeMessages = function(event){
     	var datos=event.data.split("%");
-    	datosGlobal.concat(datos+"!");
-    	alert(datosGlobal);
+    	datosGlobales=datosGlobales+event.data+"!";
     	if(event.data!="Turno"){
     		var nodoPadre=document.getElementById("contenedor");
     		var div1=document.createElement("div");
@@ -42,8 +46,10 @@ $(function(){
     		parrafo.appendChild(texto);
     		time.appendChild(persona);
     		div3.appendChild(time);
+    		sessionStorage.setItem("datosGlobales", datosGlobales);
     	}
     	else{
+    	sessionStorage.setItem("datosGlobales", datosGlobales);
         location.href="/indexr/";
     	}
     }
@@ -59,6 +65,44 @@ $(function(){
     		var texto=document.getElementById("btn-input").value;
     		socket.send(usuario+"%"+texto+"%"+game);
     		document.getElementById("btn-input").value="";
+    });
+    $(document).ready(function(event){
+    	if(datosGlobales!=""){
+    	var carga=datosGlobales.split("!");
+    	for(i=0;i<carga.length;i++){
+    		var datosL=carga[i].split("%");
+    	if(datosL[1]!=undefined){
+    	var nodoPadre=document.getElementById("contenedor");
+		var div1=document.createElement("div");
+		if(datosL[0]==datosL[2]){
+		div1.className="row msg_container base_sent";
+		}
+		else{
+			div1.className="row msg_container base_receive";
+		}
+		nodoPadre.appendChild(div1);
+		var div2=document.createElement("div");
+		div2.className="col-xs-10 col-md-10";
+		div1.appendChild(div2);
+		var div3=document.createElement("div");
+		if(datosL[0]==datosL[2]){
+		div3.className="messages msg_sent";
+		}
+		else{
+			div3.className="messages msg_receive";
+		}
+		div2.appendChild(div3);
+		var parrafo=document.createElement("p");
+		div3.appendChild(parrafo);
+		var texto=document.createTextNode(datosL[1]);
+		var time=document.createElement("time");
+		var persona=document.createTextNode(datosL[0]+"  "+new Date().getHours()+":"+new Date().getMinutes());
+		parrafo.appendChild(texto);
+		time.appendChild(persona);
+		div3.appendChild(time);
+    	}
+    	}
+    	}
     });
 });
 			})
